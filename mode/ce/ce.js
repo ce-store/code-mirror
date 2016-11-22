4f7ccd69c3c676e8a36b4965457d9cafafd5caaf
Anna Thomas <annathom@uk.ibm.com>
2016-11-22 17:54:30 +0000

    More syntax highlighting changes

--- mode/ce/ce.js
+++ mode/ce/ce.js
@@ -15,7 +15,7 @@
 
   CodeMirror.defineMode("ce", function() {
     Array.prototype.peek = function(n) {
-      if (n && n > 0 && n < this.length) {
+      if (n && n > 0 && n <= this.length) {
         return this[this.length - n];
       } else {
         return this[this.length - 1];
@@ -24,6 +24,43 @@
 
     var ch, word;
 
+    var CONCEPT = 'concept';
+
+    var addToPrevWords = function(state, word) {
+      state.prevWords.push(word.trim());
+    };
+
+    var searchFor = function(state, stream, match1, match2, type) {
+      ch = true;
+      word = '';
+      var lastWord;
+
+      if (match2) {
+        while (ch && !stream.match(match1, false) && !stream.match(match2, false)) {
+          ch = stream.next();
+          word += ch;
+
+          if (ch === ' ') {
+            lastWord = '';
+          } else {
+            lastWord += ch;
+          }
+        }
+      } else {
+        while (ch && !stream.match(match1, false)) {
+          ch = stream.next();
+          word += ch;
+        }
+      }
+
+      if (type) {
+        addToPrevWords(state, type);
+      } else {
+        addToPrevWords(state, word);
+      }
+      return lastWord;
+    };
+
     var comment = function(stream) {
       stream.skipToEnd();
       return 'comment';
@@ -31,7 +68,7 @@
 
     var keyword = function(state, words) {
       for (var i = 0; i < words.length - 1; ++i) {
-        state.prevWords.push(words[i]);
+        addToPrevWords(state, words[i]);
       }
       state.currentWord = words.peek();
 
@@ -41,32 +78,34 @@
         state.statementType = 'there is';
       }
 
-      return 'keyword';
-    };
-
-    var thereIs = function(state) {
-      state.prevWords.push('there');
-      state.currentWord = 'is';
-      state.statementType = 'there is';
-      return 'keyword';
+      // return 'keyword';
+      return null;
     };
 
     var tilde = function(state, stream) {
+      var result;
+      if (state.prevWords.peek(2) === 'conceptualise' &&
+          (state.prevWords.peek(1) === 'a' ||
+          state.prevWords.peek(1) === 'an')) {
+        result = 'atom';
+      } else {
+        result = 'operator';
+      }
       stream.skipTo('~');
       state.currentWord = '~';
-      return 'atom';
+      return result;
     };
 
     var value = function(state) {
-      state.currentWord = 'value';
-      return 'operator';
+      state.currentWord = CONCEPT;
+      return 'atom';
     };
 
-    var isA = function(state) {
-      state.prevWords.push('is');
-      state.currentWord = 'a';
-      return 'null';
-    };
+    // var isA = function(state) {
+    //   addToPrevWords(state, 'is');
+    //   state.currentWord = 'a';
+    //   return 'null';
+    // };
 
     var singleQuotes = function(state, stream) {
       stream.skipTo('\'');
@@ -82,10 +121,11 @@
 
     var fullStop = function(state) {
       if (state.currentWord.length > 0) {
-        state.prevWords.push(state.currentWord);
+        addToPrevWords(state, state.currentWord);
       }
-      state.prevWords.push('.');
+      addToPrevWords(state, '.');
       state.currentWord = '';
+      state.statementType = null;
       return null;
     };
 
@@ -96,57 +136,49 @@
     };
 
     var prevIsA = function(state, stream) {
-      ch = true;
-      word = '';
-      while (ch && !stream.match('and', false)) {
-        ch = stream.next();
-        word += ch;
-      }
-
-      state.prevWords.push(ch);
-      return 'operator';
+      searchFor(state, stream, 'and', null, CONCEPT);
+      return 'atom';
     };
 
     var prevNamed = function(state, stream) {
-      ch = true;
-      word = '';
-      while (ch && !stream.match(' ', false)) {
-        ch = stream.next();
-        word += ch;
-      }
-
-      state.prevWords.push(word);
+      searchFor(state, stream, ' ');
       return 'variable-2';
     };
 
-    var prevHasTheValue = function(state, stream) {
+    var prevHas = function(state, stream) {
       if (stream.match('the')) {
-        state.prevWords.push('the');
+        addToPrevWords(state, 'the');
         return null;
       }
 
-      ch = true;
-      word = '';
-      while (ch && !stream.match(' ', false)) {
-        ch = stream.next();
-        word += ch;
-      }
-
-      state.prevWords.push(word);
+      searchFor(state, stream, ' ');
       return 'variable-2';
     };
 
-    // var prevTilde = function(state, stream) {
-    //   ch = true;
-    //   word = '';
-    //   while (ch && !stream.match(' ', false)) {
-    //     ch = stream.next();
-    //     word += ch;
-    //   }
+    var prevThereIs = function(state, stream) {
+      searchFor(state, stream, 'named');
+      return 'atom';
+    };
 
-    //   state.prevWords.push(word);
-    //   return 'variable-2';
-    // };
+    var prevConceptualiseTilde = function(state, stream) {
+      searchFor(state, stream, ' ');
+      return 'variable-2';
+    };
+
+    var prevPropertyTilde = function(state, stream) {
+      var lastWord = searchFor(state, stream, 'and', '.');
+
+      stream.backUp(lastWord.length);
+      word = word.substring(0, word.length - lastWord.length);
+
+      addToPrevWords(state, word);
+      return 'atom';
+    };
+
+    var prevAs = function(state, stream) {
+      searchFor(state, stream, 'and', '.');
+      return 'operator';
+    };
 
     return {
       startState: function() {
@@ -169,7 +201,7 @@
 
         if (state.space) {
           if (state.currentWord.length > 0) {
-            state.prevWords.push(state.currentWord);
+            addToPrevWords(state, state.currentWord);
           }
           state.currentWord = '';
           state.space = false;
@@ -191,8 +223,10 @@
           return tilde(state, stream);
         } else if (stream.match('value')) {
           return value(state);
-        } else if (stream.match('is an') || stream.match('is a')) {
-          return isA(state);
+        // } else if (stream.match('is an')) {
+        //   return isA(state);
+        // } else if (stream.match('is a')) {
+        //   return isA(state);
         } else if (stream.match('\'')) {
           return singleQuotes(state, stream);
         } else if (stream.match('"')) {
@@ -202,74 +236,52 @@
         } else if (stream.match(' ')) {
           return space(state, stream);
         } else {
-          if (state.prevWords.peek() === 'is a') {
+          var prev1 = state.prevWords.peek(1);
+          var prev2 = state.prevWords.peek(2);
+          var prev3 = state.prevWords.peek(3);
+
+          if (prev3 !== 'there' &&
+              prev2 === 'is' &&
+              (prev1 === 'a' ||
+              prev1 === 'an')) {
             return prevIsA(state, stream);
-          } else if (state.prevWords.peek() === 'named') {
+          } else if (prev1 === 'named') {
             return prevNamed(state, stream);
-          } else if (state.prevWords.peek() === 'has' || (
-              state.prevWords.peek(3) === 'has' &&
-              state.prevWords.peek(2) === 'the' &&
-              state.prevWords.peek(1) === 'value')) {
-            return prevHasTheValue(state, stream);
-          // } else if (state.prevWords.peek(3) === 'conceptualise' &&
-          //     state.prevWords.peek(2) === 'a' &&
-          //     state.prevWords.peek(1) === '~') {
-          //   return prevTilde(state, stream);
+          } else if (prev1 === 'has') {
+          //     (prev2 === 'the' &&
+          //     prev1 === 'value')) {
+            return prevHas(state, stream);
+          } else if (prev3 === 'there' &&
+              prev2 === 'is' &&
+              (prev1 === 'a' ||
+              prev1 === 'an')) {
+            return prevThereIs(state, stream);
+          } else if (prev3 === 'conceptualise' &&
+              prev2 === 'a' &&
+              prev1 === '~') {
+            return prevConceptualiseTilde(state, stream);
+          } else if (prev2 === '~' &&
+              prev1 === 'the') {
+            return prevPropertyTilde(state, stream);
+          } else if (prev1 === 'as') {
+            return prevAs(state, stream);
           }
 
           ch = stream.next();
           state.currentWord += ch;
-          // console.log(state.prevWords);
+          console.log(state.prevWords);
         }
 
         return null;
       },
 
       indent: function(state) {
-        var prev = state.prevWords.peek();
-        if (prev === 'that' || prev === 'and') {
+        if (state.statementType) {
           return 2;
-        } else if (prev.charAt(prev.length - 1) === '.') {
-          return 0;
         } else {
-          return state.indented;
+          return 0;
         }
       }
     };
   });
 });
-
-// CodeMirror.defineSimpleMode("ce", {
-//   // The start state contains the rules that are intially used
-//   start: [
-//     // Match anything surrounded with quotes
-//     {regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: "variable"},
-//     {regex: /'(?:[^\\]|\\.)*?(?:'|$)/, token: "variable"},
-//     {regex: /(\w+)(?=\s+that)/, token: "variable"},
-//     {regex: /(\w+)(?=\s+as)/, token: "variable"},
-//     // Match anything surrounded with tildes
-//     {regex: /~(?:[^\\]|\\.)*?(?:~|$)/, token: "atom"},
-//     // Match anything before the word named
-//     {regex: /(\w+)(?=\s+named)/, token: "atom"},
-//     {regex: /(\w+)(?=\s+and)/, token: "atom"},
-//     {regex: /\w+(\s\w+)+(?=\s+(the))/, token: "atom"},
-//     // Match conceptualise
-//     {regex: /conceptualise|there is|value/, token: "keyword"},
-//     // Match capital letter variables
-//     {regex: /([A-Z]+)(?=[\s\.]+)/, token: "variable"},
-//     // Match comments
-//     {regex: /\-\-.*/, token: "comment"},
-//     // Indent after that or and
-//     {regex: /(?:that)/, indent: true},
-//     // Dedent end of sentences
-//     {regex: /\./, dedent: true},
-//   ],
-//   // The meta property contains global information about the mode. It
-//   // can contain properties like lineComment, which are supported by
-//   // all modes, and also directives like dontIndentStates, which are
-//   // specific to simple modes.
-//   meta: {
-//     dontIndentStates: ["comment"],
-//     lineComment: "--"
-//   }
-// });
