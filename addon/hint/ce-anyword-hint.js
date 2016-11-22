61ad00f4589f6176fbefb83d9f544f6c12c69dad
Anna Thomas <annathom@uk.ibm.com>
2016-11-22 16:42:17 +0000

    Add CE autocomplete

--- addon/hint/ce-anyword-hint.js
+++ addon/hint/ce-anyword-hint.js
@@ -1,16 +1,21 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
+// Edited to include CE words by Anna Thomas
+
+/* globals CodeMirror: true */
+/* globals define: true */
 
 (function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
+  'use strict';
+  if (typeof exports === "object" && typeof module === "object") {// CommonJS
     mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
+  } else if (typeof define === "function" && define.amd) { // AMD
     define(["../../lib/codemirror"], mod);
-  else // Plain browser env
+  } else {// Plain browser env
     mod(CodeMirror);
+  }
 })(function(CodeMirror) {
-  "use strict";
-  console.log('in hint');
+  'use strict';
 
   var WORD = /[\w$]+/, RANGE = 500;
 
@@ -34,13 +39,12 @@
   ];
 
   CodeMirror.registerHelper("hint", "ce", function(editor, options) {
-    console.log('in ce');
     var word = options && options.word || WORD;
     var range = options && options.range || RANGE;
     var cur = editor.getCursor(), curLine = editor.getLine(cur.line);
     var end = cur.ch, start = end;
-    while (start && word.test(curLine.charAt(start - 1))) --start;
-    var curWord = start != end && curLine.slice(start, end);
+    while (start && word.test(curLine.charAt(start - 1))) {--start;}
+    var curWord = start !== end && curLine.slice(start, end);
 
     var list = ceWords.filter(function (ceWord) {
       return ceWord.indexOf(curWord) > -1;
@@ -49,17 +53,20 @@
     var re = new RegExp(word.source, "g");
     for (var dir = -1; dir <= 1; dir += 2) {
       var line = cur.line, endLine = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
-      for (; line != endLine; line += dir) {
+      for (; line !== endLine; line += dir) {
         var text = editor.getLine(line), m;
         while (m = re.exec(text)) {
-          if (line == cur.line && m[0] === curWord) continue;
-          if ((!curWord || m[0].lastIndexOf(curWord, 0) == 0) && !Object.prototype.hasOwnProperty.call(seen, m[0]) && ceWords.indexOf(m[0]) < 0) {
+          if (line === cur.line && m[0] === curWord) {continue;}
+          if ((!curWord || m[0].lastIndexOf(curWord, 0) === 0) && !Object.prototype.hasOwnProperty.call(seen, m[0]) && ceWords.indexOf(m[0]) < 0) {
             seen[m[0]] = true;
             list.push(m[0]);
           }
         }
       }
     }
+    list.sort(function(a, b) {
+      return a.indexOf(curWord) - b.indexOf(curWord);
+    });
     return {list: list, from: CodeMirror.Pos(cur.line, start), to: CodeMirror.Pos(cur.line, end)};
   });
 });
